056f98b92eed4ce2129999a67597b8c750ea340a
mrAkshaym <akshay.mahajan.blr@gmail.com>
2024-08-26 01:01:55 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+//#include "./stl_use_algo.cpp"
+#include "./stl_use_containers.cpp"
+using namespace::std;
+
+int main()
+{
+    //cout<<"Hello World"<<endl;
+    //test_algo_sort();
+    //test_algo_search();
+    //test_algo_partition();
+    test_container_vector();
+    test_container_list();
+    return 0;
+}
